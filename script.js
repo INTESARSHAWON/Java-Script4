f12bdc7345e1fbc108927af20d0e21fecf6f0eb2
INTESARSHAWON <intesarshawon@gmail.com>
2024-03-21 20:15:40 +0600

    prompt

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //for loop
 
-for (let count=1; count<=10; count++){
+// for (let count=1; count<=10; count++){
+//     console.log("Shawon");
+// }
+
+// console.log ("loop has end");
+
+let n= prompt('type a number');
+
+for (let count=1; count<=n; count++){
     console.log("Shawon");
 }
-
-console.log ("loop has end");
-
