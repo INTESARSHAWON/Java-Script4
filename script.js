796ca2d7d1a7fedcb045116fb34b2fd2e77eb574
INTESARSHAWON <intesarshawon@gmail.com>
2024-03-21 20:16:55 +0600

    sum

--- script.js
+++ script.js
@@ -6,8 +6,16 @@
 
 // console.log ("loop has end");
 
-let n= prompt('type a number');
+// let n= prompt('type a number');
 
-for (let count=1; count<=n; count++){
-    console.log("Shawon");
-}
+// for (let count=1; count<=n; count++){
+//     console.log("Shawon");
+// }
+
+let sum=0;
+for (let i=1; i<=5; i++){
+    sum=sum+i;
+    //console.log (sum);
+}   
+    console.log ("sum=",sum);
+
